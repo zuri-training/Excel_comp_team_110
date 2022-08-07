bf2348c08810517f4a13c48a22ff8d855ee76e10
Tolupy <ajayisaiah5@gmail.com>
2022-08-07 14:09:40 +0100

    change password

--- compare_wizard/accounts/forms.py
+++ compare_wizard/accounts/forms.py
@@ -7,11 +7,7 @@ class RegisterUserForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
     last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
-    username = forms.CharField(max_length=100,
-                               required=True,
-                               widget=forms.TextInput(attrs={'placeholder': 'Username',
-                                                             'class': 'form-control',
-                                                             }))
+    username = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}))
 
     class Meta:
         model = User
@@ -54,3 +50,4 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['gender', 'country', 'avatar']
+        
