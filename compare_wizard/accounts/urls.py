30ce23fe34deeabfe28d9944a344a826c60b9cc5
Tolupy <ajayisaiah5@gmail.com>
2022-08-08 22:41:42 +0100

    reset password email solved

--- compare_wizard/accounts/urls.py
+++ compare_wizard/accounts/urls.py
@@ -1,5 +1,5 @@
 from re import template
-from django.urls import path
+from django.urls import path, include
 from . import views
 from .views import profile_user
 # from django.contrib.auth import views as auth_views
@@ -7,6 +7,7 @@ from .views import profile_user
 urlpatterns = [
     path('login_user', views.login_user, name="login"),
     path('logout_user', views.logout_user, name="logout"),
+    path('accounts/', include('allauth.urls')),
     path('register_user', views.register_user, name="register"),
     path('profile_user', views.profile_user, name="profile"),
     # path('password', auth_views.PasswordChangeView.as_view(template_name = 'registration/change-password.html')),
