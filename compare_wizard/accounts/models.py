18290ff71c70f8abeb3bf92b857422e9f11b6928
Tolupy <ajayisaiah5@gmail.com>
2022-08-09 01:43:09 +0100

    merging process

--- compare_wizard/accounts/models.py
+++ compare_wizard/accounts/models.py
@@ -1,19 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from PIL import Image
+from PIL import Image
+from django_countries.fields import CountryField
 
-# from P import image
-# from pillow import image
-
+GENDER_CHOICES = (
+    ('prefer not to say', 'PREFER NOT TO SAY'),
+    ('female', 'FEMALE'),
+    ('others', 'OTHERS'),
+    ('male', 'MALE'),
+)
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    gender = models.CharField(max_length=200, null=True, blank= True)
-    country = models.CharField(max_length=200, null=True, blank=True)
-    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    gender = models.CharField(max_length=20, choices=GENDER_CHOICES, default='prefer not to say')
+    country = CountryField(blank_label='(select country)')
+    avatar = models.ImageField(null=True, blank= True, default='default.png', upload_to='profile_images')
     
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 180 or img.width > 180:
+            new_img = (180, 180)
+            img.thumbnail(new_img)
+            img.save(self.avatar.path)
+
     def __str__(self):
         return self.user.username
+
 
     # def save(self, *args, **kwargs):
     #     super().save()
--- compare_wizard/compare_wizard/urls.py
+++ compare_wizard/compare_wizard/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = "accounts"
 app_name = "dashboard"
